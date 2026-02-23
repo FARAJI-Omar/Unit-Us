5b68232535e70847e75c6c0d627ce492771bba6b
F99 <farajiomar99@gmail.com>
2026-02-23 10:55:10 +0000

    core: Initialize Laravel application with starting config and dependencies & set up google mail stmp

---
+++ unit-us/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<span style="font-size: 24px; font-weight: bold; color: #1c1c1b;">Unit-Us</span>
+</a>
+</td>
+</tr>
